a42b38c650e1177be4b608e5d2645148cf6cb5d5
Vitor Andrade <vitorandrade@pm.me>
2021-12-03 12:36:54 +0000

    Boilerplate.

--- src/main/kotlin/org/vandrade/colloquium/ColloquiumApplication.kt
+++ src/main/kotlin/org/vandrade/colloquium/ColloquiumApplication.kt
@@ -1,9 +1,16 @@
 package org.vandrade.colloquium
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+/**
+ * Author: Vitor Andrade
+ * Date: 12/3/21
+ * Time: 12:25 PM
+ */
+
+@SpringBootApplication(exclude = [R2dbcAutoConfiguration::class])
 class ColloquiumApplication
 
 fun main(args: Array<String>) {
